15ef70d3715a64826d3033699ed9d72e09586ccd
caotrinhthinh <caotrinhthinh@gmail.com>
2025-07-19 23:34:57 +0700

    Add decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator/user.decorator';
 import { JwtAuthGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
   @UseGuards(JwtAuthGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
+  getMe(@GetUser() req: Request) {
     return req.user;
   }
 }
